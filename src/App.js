e407850fc2904e3d92fca3463e03285bdc8a69d4
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-02-02 20:22:39 +0500

    upload files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Product from "./components/Product";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,15 @@
+const Products = () => {
+  // const products = [];
+  // fetch("https://fakestoreapi.com/products")
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     products === data;
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  return <div></div>;
+};
+
+export default Products;
