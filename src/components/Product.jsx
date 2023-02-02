08a4b538d24211826b5d1b4ff5d03a11ab98b072
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-02-02 21:21:02 +0500

    upload files

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,29 +1,6 @@
 import { useState } from "react";
 
 const Product = () => {
-  // const products = [
-  //   {
-  //     id: 1,
-  //     name: "Basic Tee",
-  //     href: "#",
-  //     imageSrc:
-  //       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-  //     imageAlt: "Front of men's Basic Tee in black.",
-  //     price: "$35",
-  //     color: "Black",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Basic Tee",
-  //     href: "#",
-  //     imageSrc:
-  //       "https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg",
-  //     imageAlt: "Front of men's Basic Tee in black.",
-  //     price: "$35",
-  //     color: "Black",
-  //   },
-  //   // More products...
-  // ];
   const [products, setProducts] = useState([]);
 
   fetch("https://fakestoreapi.com/products")
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,4 @@
 const Products = () => {
-  // const products = [];
-  // fetch("https://fakestoreapi.com/products")
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     products === data;
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
-
   return <div></div>;
 };
 
