a46f00427698fc4cd6280ef35f00083db686a3e3
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-02-09 21:43:52 +0500

    upload files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Product from "./components/Product";
 import Products from "./components/Products";
+import ProductInfo from "./components/ProductInfo";
+import { Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/product:" element={<ProductInfo />} />
+      </Routes>
       <Navbar />
       <Products />
       <Product />
