7616955026712162069772a40432957e83e2f0ab
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-02-09 21:51:33 +0500

    upload files

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -51,7 +51,7 @@ const Product = () => {
                     href="#"
                     class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
-                    Add to Card
+                    <button>Add to Card</button>
                   </a>
                   <a
                     href="#"
