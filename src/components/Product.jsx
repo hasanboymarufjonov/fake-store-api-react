279423200f0d16895247e69c4f8b48118a649a05
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-02-16 01:17:56 +0500

    upload files

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -15,7 +15,7 @@ const Product = () => {
         setProducts(data);
         setLoading(false);
 
-        // console.log(data);
+        console.log(data);
       })
       .catch((err) => {
         console.log(err);
@@ -408,60 +408,28 @@ const Product = () => {
                       {product.title}
                     </h5>
                   </a>
-                  <div className="flex items-center mt-2.5 mb-5">
+
+                  <div class="flex items-center my-4">
                     <svg
                       aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
+                      class="w-5 h-5 text-yellow-400"
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <title>First star</title>
+                      <title>Rating star</title>
                       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                     </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
+                    <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+                      {product.rating.rate}
+                    </p>
+                    <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+                    <a
+                      href="#"
+                      class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"
                     >
-                      <title>Second star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Third star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Fourth star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <svg
-                      aria-hidden="true"
-                      className="w-5 h-5 text-yellow-300"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Fifth star</title>
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                    </svg>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                      5.0
-                    </span>
+                      {product.rating.count} reviews
+                    </a>
                   </div>
                   <div className="flex items-center justify-between">
                     <span className="text-xl font-bold text-gray-900 dark:text-white">
--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Navbar from "./Navbar";
+import Categories from "./Categories";
+import Footer from "./Footer";
 
 const ProductInfo = () => {
   const { productID } = useParams();
@@ -25,13 +27,13 @@ const ProductInfo = () => {
   return (
     <div>
       <Navbar />
-
+      <Categories />
       <a
         href=""
-        className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+        className="my-2 flex flex-col items-center justify-around bg-white border border-gray-200 shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
         <img
-          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          className="w-full h-96 md:height-auto md:w-96 md:rounded-lg bg-gray-300 px-20 py-10"
           src={product.image}
           alt=""
         />
@@ -39,14 +41,58 @@ const ProductInfo = () => {
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-left">
             {product.title}{" "}
           </h5>
-          <p className="text-white text-left">$ {product.price}</p>
-          <p className="text-left bg-white rounded">{product.category}</p>
+          <div class="flex items-center my-4">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-yellow-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Rating star</title>
+              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+            </svg>
+            <p class="ml-2 text-sm font-bold text-gray-900 dark:text-white">
+              {product.rating.rate}
+            </p>
+            <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+            <a
+              href="#"
+              class="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"
+            >
+              {product.rating.count} reviews
+            </a>
+          </div>
+          <div className="text-left flex">
+            <p className="pr-2 font-bold">Category: </p>
+
+            <div className="text-center bg-white rounded w-fit px-2">
+              <p>{product.category}</p>
+            </div>
+          </div>
+
+          <h2 className="font-bold text-3xl text-left mt-4">Description</h2>
+
+          <p className="mb-3 font-normal  text-left">{product.description}</p>
+          {/* <p className="text-white text-left">$ {product.price}</p> */}
+          <div className="flex items-center justify-between">
+            <span className="text-xl font-bold text-gray-900 dark:text-white">
+              $ {product.price}
+            </span>
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Add to cart
+            </a>
+          </div>
         </div>
       </a>
-      <div className="px-5">
+      {/* <div className="px-5 py-3 bg-gray-300 rounded-lg m-10">
         <h2 className="font-bold text-3xl text-left">Description</h2>
         <p className="mb-3 font-normal  text-left">{product.description}</p>
-      </div>
+      </div> */}
+      <Footer />
     </div>
   );
 };
